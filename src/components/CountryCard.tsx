d484d06812eef345cd779a9e9fdaed4253c3fdd7
oneieo <sun020716@gmail.com>
2024-06-25 22:54:40 +0900

    style: countrycards

--- src/components/CountryCard.tsx
+++ src/components/CountryCard.tsx
@@ -1,26 +1,29 @@
 import { Country } from "../types/Country.type";
 
 const CountryCard = ({ countries }: { countries: Country[] }) => {
-  console.log(countries);
-
   return (
-    <>
+    <div className="w-full h-full flex flex-col items-center content-center">
       <div>
-        <h1>Favorite Countries</h1>
+        <h1 className="font-bold text-4xl mt-5 mb-5">Favorite Countries</h1>
       </div>
-      <div>
-        <h1>Countries</h1>
-        {countries.map((country) => {
-          return (
-            <div key={country.name.common}>
-              <img src={country.flags.png} />
-              <h2>{country.name.common}</h2>
-              <p>{country.capital}</p>
-            </div>
-          );
-        })}
+      <div className="w-[1920px] flex flex-col items-center justify-center">
+        <h1 className="font-bold text-4xl mt-5 mb-5 ">Countries</h1>
+        <div className="flex flex-wrap items-center justify-center gap-10">
+          {countries.map((country) => {
+            return (
+              <div
+                key={country.name.common}
+                className="w-[300px] h-[200px] flex flex-col items-center justify-center border-2 border-slate-300 rounded-xl cursor-pointer"
+              >
+                <img src={country.flags.png} className="w-[130px] mb-2 mt-2" />
+                <h2 className="font-bold text-2xl">{country.name.common}</h2>
+                <p className="text-xl text-slate-500">{country.capital}</p>
+              </div>
+            );
+          })}
+        </div>
       </div>
-    </>
+    </div>
   );
 };
 
