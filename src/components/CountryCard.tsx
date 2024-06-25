d29b518bbc3274df10bb6811183b33544fff9481
oneieo <sun020716@gmail.com>
2024-06-25 23:19:22 +0900

    update: Creating an on-click event handler

--- src/components/CountryCard.tsx
+++ src/components/CountryCard.tsx
@@ -1,6 +1,17 @@
+import { useState } from "react";
 import { Country } from "../types/Country.type";
 
 const CountryCard = ({ countries }: { countries: Country[] }) => {
+  const [selectedCards, setSelectedCards] = useState<Country[]>([]);
+
+  const handleClickCountry = (countryName: string) => {
+    console.log(countryName);
+    const selectedCountry: Country[] = countries.find(
+      (country) => country.name.common === countryName
+    );
+    //setSelectedCards((prev) => [...prev, ...selectedCountry]);
+  };
+
   return (
     <div className="w-full h-full flex flex-col items-center content-center">
       <div>
@@ -13,7 +24,8 @@ const CountryCard = ({ countries }: { countries: Country[] }) => {
             return (
               <div
                 key={country.name.common}
-                className="w-[300px] h-[200px] flex flex-col items-center justify-center border-2 border-slate-300 rounded-xl cursor-pointer"
+                onClick={() => handleClickCountry(country.name.common)}
+                className="w-[300px] h-[200px] flex flex-col items-center justify-center border-2 border-slate-300 rounded-xl cursor-pointer hover:bg-slate-100 "
               >
                 <img src={country.flags.png} className="w-[130px] mb-2 mt-2" />
                 <h2 className="font-bold text-2xl">{country.name.common}</h2>
