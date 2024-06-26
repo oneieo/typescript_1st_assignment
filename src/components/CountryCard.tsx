02e2c5a8b38f82063cbb9171a4fec04c16773160
oneieo <sun020716@gmail.com>
2024-06-26 17:15:21 +0900

    refactor: Component content separation

--- src/components/CountryCard.tsx
+++ src/components/CountryCard.tsx
@@ -1,81 +1,21 @@
-import { useState } from "react";
 import { Country } from "../types/Country.type";
 
 const CountryCard = ({
-  countries,
-  setCountries,
+  country,
+  onClickHandler,
 }: {
-  countries: Country[];
-  setCountries: React.Dispatch<React.SetStateAction<Country[]>>;
+  country: Country;
+  onClickHandler: (name: string) => void;
 }) => {
-  // countries에 map 돌려서 isfavorite 속성 추가한 새 배열 만들어주기
-  // country 타입에도 isFavorite 속성 추가... 과제예시랑 똑같이 구현하려면!
-  const [selectedCards, setSelectedCards] = useState<Country[]>([]);
-
-  const handleClickCountry = (countryName: string) => {
-    const selectedCountry: Country | undefined = countries.find(
-      (country) => country.name.common === countryName // 일치하지 않는 경우 undefined가 뜰 수도 있음 -> 어떻게 처리할지 -> if문 사용하기
-    );
-    if (selectedCountry) {
-      setSelectedCards((prev) => [...prev, selectedCountry]);
-      const filteredCountry: Country[] = countries.filter(
-        (country) => country.name.common !== countryName
-      );
-      setCountries(filteredCountry);
-    }
-  };
-
-  const handleClickFavoriteCountry = (countryName: string) => {
-    const filteredCards: Country[] = selectedCards.filter(
-      (country) => country.name.common !== countryName
-    );
-    setSelectedCards(filteredCards);
-    const selectedFavoriteCard: Country | undefined = selectedCards.find(
-      (country) => country.name.common === countryName
-    );
-    if (selectedFavoriteCard) {
-      setCountries((prev) => [selectedFavoriteCard, ...prev]);
-    }
-  };
-
   return (
-    <div className="w-full h-full flex flex-col items-center content-center">
-      <div className="flex flex-col items-center ">
-        <h1 className="font-bold text-4xl mt-5 mb-5">Favorite Countries</h1>
-        <div className="flex flex-wrap items-center justify-center gap-10">
-          {selectedCards.map((country) => {
-            return (
-              <div
-                key={country.name.common}
-                onClick={() => handleClickFavoriteCountry(country.name.common)}
-                className="w-[300px] h-[200px] flex flex-col items-center justify-center border-2 border-slate-300 rounded-xl cursor-pointer hover:bg-slate-100 "
-              >
-                <img src={country.flags.png} className="w-[130px] mb-2 mt-2" />
-                <h2 className="font-bold text-2xl">{country.name.common}</h2>
-                <p className="text-xl text-slate-500">{country.capital}</p>
-              </div>
-            );
-          })}
-        </div>
-      </div>
-      <div className="w-[1920px] flex flex-col items-center justify-center">
-        <h1 className="font-bold text-4xl mt-5 mb-5 ">Countries</h1>
-        <div className="flex flex-wrap items-center justify-center gap-10">
-          {countries.map((country) => {
-            return (
-              <div
-                key={country.name.common}
-                onClick={() => handleClickCountry(country.name.common)}
-                className="w-[300px] h-[200px] flex flex-col items-center justify-center border-2 border-slate-300 rounded-xl cursor-pointer hover:bg-slate-100 "
-              >
-                <img src={country.flags.png} className="w-[130px] mb-2 mt-2" />
-                <h2 className="font-bold text-2xl">{country.name.common}</h2>
-                <p className="text-xl text-slate-500">{country.capital}</p>
-              </div>
-            );
-          })}
-        </div>
-      </div>
+    <div
+      key={country.name.common}
+      onClick={() => onClickHandler(country.name.common)}
+      className="w-[300px] h-[200px] flex flex-col items-center justify-center border-2 border-slate-300 rounded-xl cursor-pointer hover:bg-slate-100 "
+    >
+      <img src={country.flags.png} className="w-[130px] mb-2 mt-2" />
+      <h2 className="font-bold text-xl">{country.name.common}</h2>
+      <p className="text-lg text-slate-500">{country.capital}</p>
     </div>
   );
 };
--- src/components/CountryList.tsx
+++ src/components/CountryList.tsx
@@ -5,6 +5,10 @@ import CountryCard from "./CountryCard.js";
 
 const CountryList = () => {
   const [countries, setCountries] = useState<Country[]>([]);
+  // countries에 map 돌려서 isfavorite 속성 추가한 새 배열 만들어주기
+  // country 타입에도 isFavorite 속성 추가... 과제예시랑 똑같이 구현하려면!
+  const [selectedCards, setSelectedCards] = useState<Country[]>([]);
+
   useEffect(() => {
     const getData = async () => {
       const countriesData: Country[] = await fetchCountries();
@@ -12,9 +16,65 @@ const CountryList = () => {
     };
     getData();
   }, []);
+
+  const handleClickFavoriteCountry = (countryName: string): void => {
+    const filteredCards: Country[] = selectedCards.filter(
+      (country) => country.name.common !== countryName
+    );
+    setSelectedCards(filteredCards);
+    const selectedFavoriteCard: Country | undefined = selectedCards.find(
+      (country) => country.name.common === countryName
+    );
+    if (selectedFavoriteCard) {
+      setCountries((prev) => [selectedFavoriteCard, ...prev]);
+    }
+  };
+
+  const handleClickCountry = (countryName: string): void => {
+    const selectedCountry: Country | undefined = countries.find(
+      (country) => country.name.common === countryName // 일치하지 않는 경우 undefined가 뜰 수도 있음 -> 어떻게 처리할지 -> if문 사용하기
+    );
+    if (selectedCountry) {
+      setSelectedCards((prev) => [...prev, selectedCountry]);
+      const filteredCountry: Country[] = countries.filter(
+        (country) => country.name.common !== countryName
+      );
+      setCountries(filteredCountry);
+    }
+  };
+
   return (
     <>
-      <CountryCard countries={countries} setCountries={setCountries} />
+      <div className="w-full h-full flex flex-col items-center content-center">
+        <div className="flex flex-col items-center ">
+          <h1 className="font-bold text-3xl mt-5 mb-5">Favorite Countries</h1>
+          <div className="w-[1440px] flex flex-wrap items-center justify-center gap-5">
+            {selectedCards.map((country: Country) => {
+              return (
+                <CountryCard
+                  key={country.name.common}
+                  country={country}
+                  onClickHandler={handleClickFavoriteCountry}
+                />
+              );
+            })}
+          </div>
+        </div>
+        <div className="w-[1920px] flex flex-col items-center justify-center">
+          <h1 className="font-bold text-3xl mt-5 mb-5 ">Countries</h1>
+          <div className="w-[1440px] flex flex-wrap items-center justify-center gap-5">
+            {countries.map((country: Country) => {
+              return (
+                <CountryCard
+                  key={country.name.common}
+                  country={country}
+                  onClickHandler={handleClickCountry}
+                />
+              );
+            })}
+          </div>
+        </div>
+      </div>
     </>
   );
 };
